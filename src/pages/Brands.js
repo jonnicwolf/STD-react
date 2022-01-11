2ed52294dd415155753411841aeb0a6f8075e3a5
Jon Narine <jnwnarine@gmail.com>
2022-01-11 16:28:11 -0500

    brand links appear

--- src/pages/Brands.js
+++ src/pages/Brands.js
@@ -3,37 +3,31 @@ import {Link} from 'react-router-dom';
 
 const Brands = () => {
     return (
-        <div class=''>
+        <div>
             <Link to='https://kurvana.com/'>
-                <i source='src/assets/Kurvana.png'/>
+                <img src='src/assets/Kurvana.png' alt='kurvana'/>
             </Link>
-            <Link to='https://levelexperience.com/'>
-                <i source='src/assets/Level.png'/>
+            <Link to='https://levelexperience.com/' alt='level experience'>
+                <img src='src/assets/Level.png'/>
             </Link>
-            <Link to='https://www.monogramcompany.com/'>
-                <i source='src/assets/Monogram.png'/>
+            <Link to='https://www.monogramcompany.com/' alt='monogram'>
+                <img src='src/assets/Monogram.png'/>
             </Link>
-            <Link to='https://www.lowellfarms.com/cannabis'>
-                <i source='src/assets/Lowell Farms.png'/>
+            <Link to='https://www.lowellfarms.com/cannabis' alt='lowell farms'>
+                <img src='src/assets/Lowell Farms.png'/>
             </Link>
-            <Link to='https://dosist.com/'>
-                <i source='src/assets/Dosist.png'/>
+            <Link to='https://dosist.com/' alt='dosist'>
+                <img src='src/assets/Dosist.png'/>
             </Link>
-            <Link to='https://www.houseplant.com/'>
-                <i source='src/assets/houseplant.png'/>
+            <Link to='https://www.houseplant.com/' alt='houseplant'>
+                <img src='src/assets/houseplant.png'/>
+            </Link>            
+            <Link to='https://williesreserve.com/' alt='willies reserve'>
+                <img src='src/assets/Willys_Reserve.png'/>
             </Link>
-            {/* <Link to='/'>
-                <i source='src/assets/boricana.png'/>
-            </Link> */}
-            <Link to='https://williesreserve.com/'>
-                <i source='src/assets/Willys_Reserve.png'/>
+            <Link to='https://www.marleynatural.com/' alt='marley natural'>
+                <img src='src/assets/Marley Natural.png'/>
             </Link>
-            <Link to='https://www.marleynatural.com/'>
-                <i source='src/assets/Marley Natural.png'/>
-            </Link>
-            
-            
-            
         </div>
     )
 };
