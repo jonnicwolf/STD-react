f5805e0545d14ffcc62019c7e84df55d4ea19aa8
Jon Narine <jnwnarine@gmail.com>
2022-01-11 14:42:55 -0500

    install react v6 and export brands

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 
-import {Routes, Route, Link} from 'react-router=dom';
+import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar.js'
 
+import Brands from './pages/Brands.js';
 
 function App() {
   return (
@@ -10,12 +11,12 @@ function App() {
       <Navbar />
       
       <Routes>
+        <Route path='/brands' element={<Brands/>}/>
+        {/* <Route path='' element={</>}>
         <Route path='' element={</>}>
         <Route path='' element={</>}>
         <Route path='' element={</>}>
-        <Route path='' element={</>}>
-        <Route path='' element={</>}>
-        <Route path='' element={</>}>
+        <Route path='' element={</>}> */}
       </Routes>
     </div>
   );
--- src/pages/Brands.js
+++ src/pages/Brands.js
@@ -36,4 +36,6 @@ const Brands = () => {
             
         </div>
     )
-}
+};
+
+export default Brands;
