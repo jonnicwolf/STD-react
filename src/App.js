f4e2965dfd714440c7b92ed179431ac8eeb94ce3
Jon Narine <jnwnarine@gmail.com>
2022-02-03 20:15:47 -0500

    import pages to routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar.js'
 
+import Home from './pages/Home.js';
 import Brands from './pages/Brands.js';
+import Boricana from './pages/Boricana.js';
+import Sustainability from './pages/Sustainability.js';
+import Company from './pages/Company.js';
+import Careers from './pages/Careers.js';
 
 function App() {
   return (
@@ -11,12 +16,12 @@ function App() {
       <Navbar />
        
       <Routes>
-        <Route path='/brands' element={<Brands/>}/>
-        {/* <Route path='' element={</>}>
-        <Route path='' element={</>}>
-        <Route path='' element={</>}>
-        <Route path='' element={</>}>
-        <Route path='' element={</>}> */}
+        <Route exact path='/' component={Home}/>
+        <Route path ='/brands' component={Brands}/>
+        <Route path='/boricana' component={Boricana}/>
+        <Route path='/sustainability' component={Sustainability}/>
+        <Route path='/company' component={Company}/>
+        <Route path='/careers' component={Careers}/>
       </Routes>
     </div>
   );
