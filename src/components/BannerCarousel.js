4819cc897d5932d545e12089bcc1f3650d243ba1
Jon Narine <jnwnarine@gmail.com>
2022-02-16 23:00:25 -0500

    pointers for carousel installed

--- src/components/BannerCarousel.js
+++ src/components/BannerCarousel.js
@@ -1,7 +1,32 @@
-const BannerCarousel = ()=> {
+import React, {useState} from 'react';
+import {FaRegHandPointer} from 'react-icons/fa'
+
+const BannerCarousel = ({ slides })=> {
+    const [slide,setSlide] = useState(0)
+    const len = slides.length;
+
+    const slideRight = ()=> {
+        setSlide(slide === len - 1 ? 0 : slide + 1)
+    }
+    const slideLeft = ()=> {
+        setSlide(slide === len -1 ? 0 : slide -1)
+    }
     return (
-        
+        <>
+        <FaRegHandPointer />
+        <FaRegHandPointer />
+
+        {slides.map((slide, index) => {
+            return (
+              <div key={index}>
+                {index === slide}
+              </div>
+            );
+          })}
+        </>
     )
 };
 
 export default BannerCarousel;
+
+
--- src/pages/Boricanna.js
+++ src/pages/Boricanna.js
@@ -1,8 +1,27 @@
 import Navbar from "../components/Navbar.js"
-
+import BannerCarousel from "../components/BannerCarousel.js";
+const slides = [
+    {
+        image:
+          "https://images.unsplash.com/photo-1546190255-451a91afc548?ixlib=rb-1.2.1"
+      },
+      {
+        image:
+          "https://images.unsplash.com/photo-1591348122449-02525d70379b?ixlib=rb-1.2.1"
+      },
+      {
+        image:
+          "https://images.unsplash.com/photo-1548802673-380ab8ebc7b7?ixlib=rb-1.2.1"
+      },
+      {
+        image:
+        "https://images.unsplash.com/photo-1577023311546-cdc07a8454d9?ixlib=rb-1.2.1"
+      }
+]
 const Boricanna = () => {
     return (
         <div>
+            <BannerCarousel slides={slides}/>
             <p>
                 yoyoyo
             </p>
