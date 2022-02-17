15b7571a4d6f2dd48ff4e648808f1250b46b5581
Jon Narine <jnwnarine@gmail.com>
2022-02-17 05:02:12 -0500

    housekeeping

--- src/components/BannerCarousel.js
+++ src/components/BannerCarousel.js
@@ -1,29 +1,30 @@
 import React, {useState} from 'react';
-import {FaRegHandPointer} from 'react-icons/fa'
+import { SlideImage, StyledSlider } from '../styledComponents/SlideContainer';
+import {FaHandPointLeft,FaHandPointRight} from 'react-icons/fa'
 
 const BannerCarousel = ({ slides })=> {
     const [slide,setSlide] = useState(0)
     const len = slides.length;
 
     const slideRight = ()=> {
-        setSlide(slide === len - 1 ? 0 : slide + 1)
+        setSlide(slide === len -1 ? 0 : slide + 1)
     }
     const slideLeft = ()=> {
         setSlide(slide === len -1 ? 0 : slide -1)
     }
     return (
-        <>
-        <FaRegHandPointer />
-        <FaRegHandPointer />
+        <StyledSlider>
+        <FaHandPointLeft className='leftArrow' onClick={slideLeft}/>
+        <FaHandPointRight className='rightArrow' onClick={slideRight}/>e
 
         {slides.map((slide, index) => {
             return (
-              <div key={index}>
-                {index === slide}
-              </div>
+                <div key={index}>
+                    {index === slide}
+                </div>
             );
-          })}
-        </>
+        })}
+        </StyledSlider>
     )
 };
 
