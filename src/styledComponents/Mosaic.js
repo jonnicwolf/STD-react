09cd7ea4d2de5a07ec06ec2f4177659a9c0b5ae6
Jon Narine <jnwnarine@gmail.com>
2022-02-18 16:53:59 -0500

    topbar tweaks

---
+++ src/styledComponents/Mosaic.js
@@ -0,0 +1,5 @@
+import StyledComponent from "styled-components";
+
+export const Mosaic = styled.div`
+    
+`
---
+++ src/styledComponents/TopMenu.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Logos = styled.ul`
+    display: flexbox;
+    flex-direction: row;
+    justify-content: flex-start;
+`;
+
+export const Menu = styled.li`
+
+`;
