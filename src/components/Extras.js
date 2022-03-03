c33881545a8f6e2336877972344b83cde852209d
Jon Narine <jnwnarine@gmail.com>
2022-03-03 05:09:40 -0500

    extras list scaffold

--- src/components/Extras.js
+++ src/components/Extras.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import mockup4 from '../photo_assets/Flavours/mockup4.jpg';
-import mockup5 from '../photo_assets/Flavours/mockup5.jpg';
-import mockup6 from '../photo_assets/Flavours/mockup6.jpg';
+
+import './Extras.css'
 
 const Extras = ({items}) => {
   return (
     <div className='extras-master-container'>
         <ul className='extras-container'>
-            {items.map((item) => (
-                <li className='extras-child' key={'foo'}>
-                    <img src={item.item} alt=""/>
+            {items.map((item,idx) => (
+                <li className='extras-child' key={idx}>
+                    <img className='extras-item' src={item.item} alt=""/>
                     <p>{item.info}</p>
                 </li>
             ))}
         </ul>
     </div>
   )
-}
+};
 
-export default Extras
+export default Extras;
 
