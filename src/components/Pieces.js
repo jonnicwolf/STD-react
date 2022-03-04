1dee01c3eec35c9cba773688efccab5cd7c4ecf1
Jon Narine <jnwnarine@gmail.com>
2022-03-04 03:58:42 -0500

    housekeeping

---
+++ src/components/Pieces.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const items  = [
+    {item:'' , image:''},
+    {item:'' , image:''},
+    {item:'' , image:''},
+  ]//back end smokables
+  
+
+const Pieces = (items) => {
+  return (
+    <div>Pieces</div>
+  )
+}
+
+export default Pieces
