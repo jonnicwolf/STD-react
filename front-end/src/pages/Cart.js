d36d1e2a81804f05b773199636e0d6203c571d0d
Jon Narine <jnwnarine@gmail.com>
2022-04-20 18:03:33 -0400

    work on subtotal

--- front-end/src/pages/Cart.js
+++ front-end/src/pages/Cart.js
@@ -1,14 +1,47 @@
 import React from 'react'
 import CartComp from '../components/CartComp'
 
-const items = ['joint','bong','blunt','skunt']
+// const items = ['joint','bong','blunt','skunt']
+const items = [
+  {
+    itemName: 'joint',
+    inStock: true,
+    inventory: 100_000,
+    itemStrain: 'Blue Dream',
+    itemSize: 'King',
+    price: 5
+  },
+  {
+    itemName: 'bong',
+    inStock: false,
+    inventory: 100_000,    
+    itemSize: 'Large',
+    price: 120
+  },
+  {
+    itemName: 'blunt',
+    inStock: true,
+    inventory: 100_000,
+    itemStrain: 'Biscotti',
+    itemSize: 'King',
+    price: 12
+  },
+  {
+    itemName: 'grinder',
+    inStock: true,
+    inventory: 100_000,    
+    itemSize: 'Jack',
+    price: 20
+  }
+]
 const Cart = () => {
-
+  
   return (
     <div>
         <h1>Shopping Cart</h1>
         <CartComp items={items}/>
         {/* add subtotal */}
+        <div>${items.reduce((acc,curVal)=> acc.price + curVal.price)}.00</div>
         <button>Checkout</button>
     </div>
   )
