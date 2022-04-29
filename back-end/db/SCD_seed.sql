7639c471167878b0e95d356703ea1784d4cf3089
Jon Narine <jnwnarine@gmail.com>
2022-04-29 16:05:14 -0400

    db seed data

---
+++ back-end/db/SCD_seed.sql
@@ -0,0 +1,26 @@
+\c stonedcolddreamery_db;
+
+INSERT INTO smokes (
+    size, strain, brand, price
+) 
+VALUES     
+    ('¾','Blue Dream','Boricanna',7),
+    ('¾','Girl Scout Cookies','Boricanna',5),
+    ('King','Biscotti','Boricanna',12),
+    ('King','London Pound Cake','Boricanna'12);
+
+INSERT INTO tokes (
+    size, brand, material, price
+) VALUES
+    ('100ml','HousePlant','Silicion',30),
+    ('100ml','Boricanna','Glass',100),
+    ('200ml','Boricanna','Glass',150),
+    ('10ml','Boricanna','Glass',20),
+    ('12ml','Boricanna','Silicion',8);
+
+INSERT INTO accessories (
+    size, brand, price
+) VALUES
+    ('Small','Boricanna','Stainless Steel', 50),
+    ('Medium','Boricanna','Aluminum',80),
+    ('King','Boricanna','Titanium',150);
