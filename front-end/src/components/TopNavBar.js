09ce86cc9a0f466e16f85eaa1722e6a4c0f9b681
Jon Narine <jnwnarine@gmail.com>
2022-07-19 04:12:01 -0400

    housekeeping

--- front-end/src/components/TopNavBar.js
+++ front-end/src/components/TopNavBar.js
@@ -27,35 +27,47 @@ import './TopNavBar.css';
 const TopNavBar = ()=> {
   return (
     <nav className='topnav-masterContainer'>            
-      <ul className='topnav-container'>                
+      <ul className='topnav-container'>  
+
         <li className='topnav-child'>
-          <Link to='/products' className='topnav-child-button'>
-            Products
-          </Link>
-        </li>        
-        <li className='topnav-child'>            
-          <Link to='/brands' className='topnav-child-button'>
-            Our Brands
-          </Link>          
+          <ul id='products-ourBrands'>
+            <li className='topnav-child'>
+              <Link to='/products' className='topnav-child-button'>
+                Products
+              </Link>
+            </li>        
+            <li className='topnav-child'>            
+              <Link to='/brands' className='topnav-child-button'>
+                Our Brands
+              </Link>          
+            </li>
+          </ul>
         </li>
+
         <li className='topnav-child-logotray'>            
           <LogoTray />
         </li>
-        <li className='topnav-child'>            
-          <Link to='/boricanna' className='topnav-child-button'>
-            Boricanna
-          </Link>          
+
+        <li className='topnav-child'>
+          <ul id='boricanna-account-cart'>
+            <li>            
+              <Link to='/boricanna' className='topnav-child-button'>
+                Boricanna
+              </Link>          
+            </li>
+            <li>            
+              <Link to='/account' className='topnav-child-button'>
+                Account
+              </Link>          
+            </li>                        
+            <li>            
+              <Link to='/cart' className='topnav-child-button'>
+                🛒
+              </Link>          
+            </li>
+          </ul>
         </li>
-        <li className='topnav-child'>            
-          <Link to='/account' className='topnav-child-button'>
-            Account
-          </Link>          
-        </li>                        
-        <li className='topnav-child'>            
-          <Link to='/cart' className='topnav-child-button'>
-            🛒
-          </Link>          
-        </li>                        
+
       </ul>       
     </nav>
   )
